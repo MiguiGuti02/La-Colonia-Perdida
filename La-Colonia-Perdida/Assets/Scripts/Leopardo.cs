1b395f49807bd9e6d11929e9fb96f48ba721e67d
RodrigoMenendezTrejo <menendez.trejo.rodrigo@gmail.com>
2023-10-25 18:33:09 +0200

    Cambiado Script Movimiento Leopardo

--- La-Colonia-Perdida/Assets/Scripts/Leopardo.cs
+++ La-Colonia-Perdida/Assets/Scripts/Leopardo.cs
@@ -1,32 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Leopardo : MonoBehaviour
-{   
-    float velocidad = 2f;
-    float rango = 4f;
-    float posIni;
-    Vector2 direccion;
+{
+    public float velocidad = 3.0f; // Velocidad de movimiento del Leopardo
+    private bool moviendoseHaciaDerecha = true; // Indicador de dirección
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        posIni=transform.position.x;
-        direccion=Vector2.left;
+        // Mover el Leopardo
+        if (moviendoseHaciaDerecha)
+        {
+            transform.Translate(Vector3.right * velocidad * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector3.left * velocidad * Time.deltaTime);
+        }
+
+        // Cambiar dirección al llegar a un límite
+        if (transform.position.x >= 14.5f) // Ajusta el límite derecho según tus necesidades
+        {
+            CambiarDireccion();
+        }
+        else if (transform.position.x <= 0.0f) // Ajusta el límite izquierdo según tus necesidades
+        {
+            CambiarDireccion();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Cuando colisiona con el jugador
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        transform.Translate(direccion*velocidad*Time.deltaTime);
-        if(Mathf.Abs(transform.position.x - posIni) >= rango){
-            Girar();
-        } 
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("El Leopardo ha colisionado con el jugador.");
+            CambiarDireccion(); // Cambia de dirección al colisionar con el jugador
+        }
     }
 
-    void Girar()
+    // Método para cambiar la dirección
+    private void CambiarDireccion()
     {
-        direccion = -direccion;        
+        moviendoseHaciaDerecha = !moviendoseHaciaDerecha;
     }
 }
