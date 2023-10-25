878e7f8628c17b14697aeca5c78a52e83f906d3f
RodrigoMenendezTrejo <menendez.trejo.rodrigo@gmail.com>
2023-10-25 18:18:28 +0200

    Camara a lo Mario bros

--- La-Colonia-Perdida/Assets/Scripts/Camara.cs
+++ La-Colonia-Perdida/Assets/Scripts/Camara.cs
@@ -1,21 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Camara : MonoBehaviour
 {
-    public Transform jugador;
+    public Transform target; // Referencia al Transform del jugador
+    public float smoothSpeed = 0.125f;
     public Vector3 offset;
-    // Start is called before the first frame update
-    void Start()
+
+    void LateUpdate()
     {
-        
+        Vector3 desiredPosition = new Vector3(target.position.x + offset.x, transform.position.y, transform.position.z);
+        transform.position = desiredPosition;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 posicion = jugador.position+offset;
-        transform.position = new Vector3(posicion.x,posicion.y,posicion.z);
-    }
 }
